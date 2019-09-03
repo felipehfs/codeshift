394a7c2704ae7facae0926ea6da37c10b8d98ad4
Felipe Henrique Ferreira sousa <felipehfsouza@gmail.com>
2019-09-03 12:52:13 -0300

    add travis.yml

--- api/config/express.js
+++ api/config/express.js
@@ -5,15 +5,18 @@ const { MONGO_URL } = require("./database");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const upload = require("express-fileupload");
+const morgan = require("morgan");
 
 const routes = require("../routes");
 const jwtauth = require("./passport");
 
+app.use('api/static', express.static('/tmp'));
 app.use(upload());
 app.use(passport.initialize());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(routes);
+app.use(morgan('tiny'));
 
 jwtauth(passport);
 
--- api/models/user.js
+++ api/models/user.js
@@ -12,8 +12,7 @@ const userSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: true,
-    minlength: 8
+    required: true
   }
 });
 
